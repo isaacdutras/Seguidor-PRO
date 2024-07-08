45348e3dd6c3a1eaba2e3dbe40b226f77b9f89e0
Dutras2017 <isaacdutra2017@gmail.com>
2024-07-07 22:40:14 -0400

    Finzalizado máquina de estados para cruzamnetos, falta somente testes dos motores

--- Seguidor_PRO_ESP32/src/main.cpp
+++ Seguidor_PRO_ESP32/src/main.cpp
@@ -20,21 +20,24 @@ double calculoPid();
 void controlePid();
 void controleMotor(int vel_M1, int vel_M2);
 void contagem_de_voltas();
+void deteccao_faixa();
 
-const int BITS = 10;     // Define a resolucao do pwm para 10 Bits (2^10 ou 1024)
+const int BITS = 10;    // Define a resolucao do pwm para 10 Bits (2^10 ou 1024)
 const int FREQ = 10000; // Define a frequencia do pwm
 const int IN1_chanel = 0;
 const int IN2_chanel = 1;
 const int IN3_chanel = 2;
 const int IN4_chanel = 3;
 
-const int velMax_M1 = 1080 + OFFSET;
-const int velMax_M2 = 1080;
+int velMax_M1 = 1000 + OFFSET;
+int velMax_M2 = 1000;
+int velMin_M1 = 664 + OFFSET;
+int velMin_M2 = 664;
 
-const int velMin_M1 = 750 + OFFSET;
-const int velMin_M2 = 750;
+// Setpoint 8 sensores: 3500
+// Setpoint 6 sensores: 2365
 
-const int setpoint = 3500;
+const int setpoint = 2365;
 
 const uint8_t NUM_SENSORES = 6;
 uint16_t valorSensores[NUM_SENSORES];
@@ -46,8 +49,9 @@ unsigned long int tempo;
 
 int ultimo_val_sensor = 0;
 
-double kp_c = 5, ki_c = 0, kd_c = 4.2;
-double kp = 0.13, ki = 0, kd = 0.8;
+double kp_c = 0.28, ki_c = 0, kd_c = 0;
+double kp = 0.08, ki = 0, kd = 0.06;
+// Varriaveis 8 sensores: kp = 0.13, ki = 0, kd = 0.8
 
 bool curva = 0;
 
@@ -56,7 +60,15 @@ int cont_voltas = 0;
 
 bool Linha_preta = true;
 
+bool state_esquerdo = 0;
+bool state_direito = 0;
+
 QTRSensors qtr; // Objeto qtr
+
+unsigned long int tempo_sensor_parada = 0;
+unsigned long int tempo_sensor_curva = 0;
+
+int var = 0;
 
 void setup()
 {
@@ -83,66 +95,53 @@ void setup()
   ledcSetup(IN3_chanel, FREQ, BITS);
   ledcSetup(IN4_chanel, FREQ, BITS);
 
-  controleMotor(0,0);
-  digitalWrite(SLEEP, 0);
-  calibrar();
-} 
+  controleMotor(0, 0);
+}
 
 void loop()
 {
 
-  int var = qtr.readLineBlack(valorSensores);
+  unsigned int tempo_pressionado = 0;
 
-  for(int i = 0; i < NUM_SENSORES; i++)
+  if (!digitalRead(BOTAO_CONTROLE))
   {
-    Serial.print(valorSensores[i]);
-    Serial.print('\t');
+    tempo = millis();
+
+    while (!digitalRead(BOTAO_CONTROLE))
+    {
+      tempo_pressionado = millis() - tempo;
+    }
   }
 
-  Serial.println(var);
+  while (tempo_pressionado <= 500 && tempo_pressionado != 0)
+  {
 
-  // unsigned int tempo_pressionado = 0;
+    if (digitalRead(FALL) && cont_voltas < n_voltas + 1)
+    {
+      digitalWrite(SLEEP, 1);
+      deteccao_faixa();
+      controlePid();
+    }
 
-  // if (!digitalRead(BOTAO_CONTROLE))
-  // {
-  //   tempo = millis();
+    else
+    {
+      digitalWrite(SLEEP, 0);
+      curva = 0;
+      cont_voltas = 0;
+      break;
+    }
+  }
 
-  //   while (!digitalRead(BOTAO_CONTROLE))
-  //   {
-  //     tempo_pressionado = millis() - tempo;
-  //   }
-  // }
-
-  // while(tempo_pressionado <= 500 && tempo_pressionado != 0)
-  // {
-    
-  //   if (digitalRead(FALL) && cont_voltas < n_voltas + 1)
-  //   {
-  //     digitalWrite(SLEEP, 1);
-  //     controlePid();
-  //     contagem_de_voltas();
-  //   }
-
-  //   else
-  //   {
-  //     digitalWrite(SLEEP, 0);
-  //     cont_voltas = 0;
-  //     break;
-  //   }
- 
-  // }
-
-  // if(tempo_pressionado >= 1200 && tempo_pressionado != 0)
-  // {
-  //   calibrar();
-  // }
-
+  if (tempo_pressionado >= 1200 && tempo_pressionado != 0)
+  {
+    calibrar();
+  }
 }
 
 void calibrar()
 {
   qtr.setTypeAnalog();
-  qtr.setSensorPins((const uint8_t[]){14, 27, 26, 25, 33, 32}, NUM_SENSORES);
+  qtr.setSensorPins((const uint8_t[]){27, 26, 25, 33, 32, 35}, NUM_SENSORES);
   qtr.setEmitterPin(12);
 
   delay(500);
@@ -183,49 +182,59 @@ void controlePid()
 
   double pid;
 
-  int val_sensor = qtr.readLineBlack(valorSensores);
+  static int linha_esquerda = 0;
 
-  // if (digitalRead(SENSOR_CURVA) == Linha_preta)
-  // {
-  //   curva = !curva;
+  int vel_M1;
+  int vel_M2;
 
-  //   while (digitalRead(SENSOR_CURVA));
-    
-  // }
-  // if (digitalRead(SENSOR_PARADA) == Linha_preta)
-  // {
-  //   cont_voltas++;
-  //   while (digitalRead(SENSOR_PARADA) == Linha_preta);
-    
-  // }
-  // else if(digitalRead(SENSOR_PARADA) == Linha_preta && digitalRead(SENSOR_CURVA) == Linha_preta);
+  uint16_t val_sensor = qtr.readLineBlack(valorSensores);
 
-  // if (valorSensores[0] > 900)
-  // {
-  //   val_sensor = 0;
-  // }
-  // if (valorSensores[7] > 900)
-  // {
-  //   val_sensor = 7000;
-  // }
+  if (curva == 1 /*&& (valorSensores[2] < 900 && valorSensores[3] < 900)*/)
+  {
+    pid = calculoPid(val_sensor, kp_c, ki_c, kd_c);
 
-  // if (curva == 1 && valorSensores[3] < 900 && valorSensores[4] < 900)
-  // {
-  //   pid = calculoPid(val_sensor, kp_c, ki_c, kd_c);
-  // }
-  // else if (curva ==  0 && valorSensores[3] > 900 && valorSensores[4] > 900)
-  // {
-  //   pid = calculoPid(val_sensor, kp, ki, kd);
-  // }
-  // else
-  // {
-  //   pid = calculoPid(val_sensor, kp_c, ki_c, kd_c);
-  // }
+    if (vel_M1 < velMin_M1 - 20)
+    {
+      vel_M1 = -velMin_M1 - pid;
+    }
 
-  pid = calculoPid(val_sensor, kp, ki, kd);
-  
-  int vel_M1 = velMin_M1 - pid;
-  int vel_M2 = velMin_M2 + pid;
+    if (vel_M2 < velMin_M2 - 20)
+    {
+      vel_M2 = -velMin_M2 + pid;
+    }
+  }
+  else if (curva == 0 && (valorSensores[0] < 900 || valorSensores[5] < 900))
+  {
+    pid = calculoPid(val_sensor, kp, ki, kd);
+
+    if (vel_M1 < 0)
+    {
+      vel_M1 = velMin_M1 - pid;
+    }
+
+    if (vel_M2 < 0)
+    {
+      vel_M2 = velMin_M2 + pid;
+    }
+  }
+
+  else
+  {
+    pid = calculoPid(val_sensor, kp_c, ki_c, kd_c);
+
+    if (vel_M1 < velMin_M1 - 70)
+    {
+      vel_M1 = -velMin_M1 - pid;
+    }
+
+    if (vel_M2 < velMin_M2 - 70)
+    {
+      vel_M2 = -velMin_M2 + pid;
+    }
+  }
+
+  vel_M1 = velMin_M1 - pid;
+  vel_M2 = velMin_M2 + pid;
 
   if (vel_M1 > velMax_M1)
   {
@@ -236,18 +245,7 @@ void controlePid()
     vel_M2 = velMax_M2;
   }
 
-  if (vel_M1 < 0)
-  {
-    vel_M1 = 0;
-  }
-  
-  if (vel_M2 < 0)
-  {
-    vel_M2 = 0;
-  }
-
   controleMotor(vel_M1, vel_M2);
-
 }
 
 void controleMotor(int vel_M1, int vel_M2)
@@ -256,40 +254,107 @@ void controleMotor(int vel_M1, int vel_M2)
   {
     ledcWrite(IN1_chanel, 0);
     ledcWrite(IN2_chanel, vel_M1);
-    Serial.print("M1: ");
-    Serial.print(vel_M1);
-    Serial.println('\t');
-
+    // Serial.print("M1: ");
+    // Serial.print(vel_M1);
+    // Serial.print('\t');
   }
   else
   {
     ledcWrite(IN1_chanel, -vel_M1);
     ledcWrite(IN2_chanel, 0);
-    Serial.print("M1: ");
-    Serial.print(-vel_M1);
-    Serial.println('\t');
+    // Serial.print("M1: ");
+    // Serial.print(-vel_M1);
+    // Serial.print('\t');
   }
 
   if (vel_M2 > 0)
   {
     ledcWrite(IN3_chanel, vel_M2);
     ledcWrite(IN4_chanel, 0);
-    Serial.print("M2: ");
-    Serial.println(vel_M2);
+    // Serial.print("M2: ");
+    // Serial.print(vel_M2);
+    // Serial.println('\t');
   }
   else
   {
     ledcWrite(IN3_chanel, 0);
     ledcWrite(IN4_chanel, -vel_M2);
-    Serial.print("M2: ");
-    Serial.println(-vel_M2);
+    // Serial.print("M2: ");
+    // Serial.print(-vel_M2);
+    // Serial.print('\t');
+    // Serial.print(cont_voltas);
+    // Serial.println('\t');
   }
 }
 
-void contagem_de_voltas()
+void deteccao_faixa()
 {
-  if (digitalRead(SENSOR_PARADA) == Linha_preta)
+  static int sensor_prioritario = 0;
+
+  if (digitalRead(SENSOR_CURVA) == Linha_preta && state_esquerdo == 0)
   {
+    if (sensor_prioritario == 0 && state_direito == 0)
+    {
+      tempo_sensor_curva = millis();
+      sensor_prioritario = 1;
+    }
+
+    state_esquerdo = 1;
 
   }
+  else if (digitalRead(SENSOR_CURVA) != Linha_preta && state_esquerdo == 1)
+  {
+    state_esquerdo = 0;
+  }
+
+  while(millis() - tempo_sensor_curva < 100)
+  {
+    if(digitalRead(SENSOR_PARADA) == Linha_preta && state_direito == 0)
+    {
+      sensor_prioritario = 0;
+      state_direito = 1;
+      break;
+    }
+    
+  }
+
+  if(millis() - tempo_sensor_curva > 100 && sensor_prioritario == 1 )
+  {
+    curva = !curva;
+    sensor_prioritario = 0;
+  }
+  
+// //------------------------------------------------------------------------------
+
+  if (digitalRead(SENSOR_PARADA) == Linha_preta && state_direito == 0)
+  {
+    if (sensor_prioritario == 0 && state_esquerdo == 0)
+    {
+      tempo_sensor_parada = millis();
+      sensor_prioritario = 2;
+    }
+    state_direito = 1;
+  }
+
+  else if (digitalRead(SENSOR_PARADA) == !Linha_preta && state_direito == 1)
+  {
+    state_direito = 0;
+  }
+
+  while(millis() - tempo_sensor_parada < 100)
+  {
+    if(digitalRead(SENSOR_CURVA) == Linha_preta && state_esquerdo == 0)
+    {
+      sensor_prioritario = 0;
+      state_esquerdo = 1;
+      break;
+    }
+  
+  }
+  if(millis() - tempo_sensor_parada > 100 && sensor_prioritario == 2)
+  {
+    cont_voltas++;
+    sensor_prioritario = 0;
+  }
+  
 }
